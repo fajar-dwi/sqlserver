9f7b1c22b2ba259ea21cd8770f934ae86969407e
fajar-dwi <fajardwi60xxx@gmail.com>
2019-11-13 23:38:48 +0700

    update asus

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','id_role',
+        'name', 'email', 'password',
     ];
 
     /**
@@ -36,8 +36,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    public function role()
-    {
-        return $this->belongsTo(Role::class);
-    }
 }
