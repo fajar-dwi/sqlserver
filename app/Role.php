668d78aa3b68364e5fd7331a08b9eb89038ab2c9
fajar-dwi <fajardwi60xxx@gmail.com>
2019-11-13 11:01:19 +0700

    update

---
+++ app/Role.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+  protected $table='table_role';
+  public function user()
+  {
+    return $this->hasMany(User::class);
+  }
+}
