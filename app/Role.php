81bc4c7536d06858ed5b245df0280dfc02e8b6cc
fajar-dwi <fajardwi60xxx@gmail.com>
2019-11-13 11:10:03 +0700

    update fajar

--- app/Role.php
+++ app/Role.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-  protected $table='table_role';
-  public function user()
-  {
-    return $this->hasMany(User::class);
-  }
+    protected $table = 'table_role';
+
+    public function user()
+    {
+        return $this->hasMany(User::class);
+    }
 }
