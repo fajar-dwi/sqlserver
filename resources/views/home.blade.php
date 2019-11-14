855f2c699b11d34d7f7a26d9b448ef4515550140
fajar-dwi <fajardwi60xxx@gmail.com>
2019-11-14 09:38:46 +0700

    update kantor

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,7 @@
-@extends('layouts.app')
-
+@extends('layouts.main')
+@section('title','home')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @role('admin')
-                        gw admin
-                    @else
-                        gw user
-                    @endrole
-                </div>
-            </div>
-        </div>
+    <div class="container-fluid">
+        
     </div>
-</div>
 @endsection
